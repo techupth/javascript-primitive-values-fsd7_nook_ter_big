1a7fd8e77c3c088fdb621dc8277f0b5478273e3f
TheYami <luffy90san@gmail.com>
2024-09-30 14:02:52 +0700

    shopping and Discount

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,7 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let allPrice = 27000;
+const discount = (allPrice * 25)/100 ;
+let totalPrice = allPrice - discount;
+console.log(totalPrice);
